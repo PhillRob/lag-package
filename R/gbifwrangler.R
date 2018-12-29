b23af3cf10219a1de7113500d1aa7b9c2c732d9c
PhillRob <robeck-p@email.ulster.ac.uk>
2018-12-29 18:08:35 +1100

    update wrangler

--- R/gbifwrangler.R
+++ R/gbifwrangler.R
@@ -11,74 +11,77 @@
 #'
 #' @examples
 #' \dontrun{
-#'  gbifwrangler(x)
+#'  gbifwrangler(x, year=2017, minocc = 15, noyears=5)
 #' }
 #'
 #'
 
 gbifwrangler <- function(x, year=2017, minocc = 15, noyears=5){
-
+  if (!require(plyr)) install.packages('plyr')
+  library(plyr)
+  #x<-MDG
   # filter data
   x <- x[!is.na(x$year),] # remove all records without time stamp
 
-  if (year <= 2018) and (year >= 0){
-    x <- x[(USA.occ.df.y.ll$year<year),]
-  else
-    stop("year value is not between 0 and 2018")
-  }
+  if (year <= 2018 & year >= 0) {
+      x <- x[(x$year < year),]
+    } else {
+      stop("year value is not between 0 and 2018")
+    }
 
-  if (minnocc >= 1){
-    x <- subset(x, with(x, unsplit(table(species), species)) >= minocc)
-  else
+  if (minocc >= 1) {
+    x <- subset(x, with(x, unsplit(table(name), name)) >= minocc)
+  } else {
     stop("minoc must be larger or equal to 1")
   }
-  if (noyear >0){
-    x.noyear <- by(x, x$species, function(x) (nrow(count(x[,c("year")])) < noyear) == T)
-    x.noyear <- ldply(x.noyear, data.frame)
-    x.noyear <- x.noyear[(x.noyear$X..i.. == TRUE),1]
-    x <- x[!(x$species %in% x.noyear),]
-    else
-      continue
+
+  if (noyears > 0) {
+    x.noyears <-
+      by(x, x$name, function(x)
+        (nrow(count(x[, c("year")])) < noyears) == T)
+    x.noyears <- ldply(x.noyears, data.frame)
+    x.noyears <- x.noyears[(x.noyears$X..i.. == TRUE), 1]
+    x <- x[!(x$name %in% x.noyears),]
+  } else {
+    continue
   }
 
-# remove species with less than 5 occurence years
-sp.5 <-
-  by(USA.occ.df.y.ll.12.15, USA.occ.df.y.ll.12.15$species, function(x)
-    (nrow(count(x[,c("year")])) < 7) == T)
-sp.5.df <- ldply(sp.5, data.frame)
-sp.5.df.1 <- sp.5.df[(sp.5.df$X..i.. == TRUE),1]
-USA.occ.df.y.ll.12.15.5 <-
-  USA.occ.df.y.ll.12.15[!(USA.occ.df.y.ll.12.15$species %in% sp.5.df.1),]
+  x <- x[c("name", "year")]
 
-nrow(USA.occ.df.y.ll.12.15.5)
-USA.l <- USA.occ.df.y.ll.12.15.5[c("species", "year")]
+  # time series
+  x.freq <- as.data.frame(table(x$name, x$year))
+  colnames(x.freq) <- c("name", "year")
 
-# freq
-f_y_sp <- as.data.frame(table(USA.l$species, USA.l$year))
-colnames(f_y_sp) <- c("species", "year")
-df_n1 <- merge(USA.l, f_y_sp, by = c("species", "year"))
-df_n2 <- unique(df_n1[, 1:3])
-colnames(df_n2) <- c("Species", "Year", "Fre")
-f_s_cum_n <- aggregate(Fre ~ Species, df_n2, function(x) cumsum(x))
-df_n3 <- cbind(df_n2, as.vector(unlist(f_s_cum_n[2])))
-colnames(df_n3) <- c("Species", "Year","Fre", "Cum")
-#unique(IR.ALL.df$speciesKey)
-f_y <- aggregate(Fre ~ Year, df_n3, function(x) sum(x))
-df4 <- merge(df_n3, f_y, by = "Year")
-df5 <- df4[order(df4$Species, df4$Year), ]
+  x.freq.1 <- merge(x, x.freq, by = c("name", "year"))
+  x.freq.1 <- unique(x.freq.1[, 1:3])
+  colnames(x.freq.1) <- c("Species", "Year", "Fre")
 
+  x.freq.2 <-
+    aggregate(Fre ~ Species, x.freq.1, function(x)
+      cumsum(x))
+  x.freq.3 <- cbind(x.freq.1, as.vector(unlist(x.freq.2[2])))
 
-# prepare for lag
-df5$Island<-"North"
-df5 <- df5[c(6,2,1,3,4,5)]
-colnames(df5) <- c("Island","Species","Year", "COUNT", "CUM_FREQ", "No_specimen_year")
-df5 <- df5[order(df5$Island, df5$Species, df5$Year), ]
+  colnames(x.freq.3) <- c("Species", "Year", "Fre", "Cum")
 
-f <- as.data.frame(seq.int(from = (f_y[1, 1]), to = (f_y[as.numeric(length(f_y$Fre)),
-                                                         1])))
-colnames(f)[1] <- "Year"
-f1 <- merge(f_y, f, all = TRUE, by = "Year")
-f1$Fre[is.na(f1$Fre)] <- 0
-colnames(f1) <- c("Year", "No_speciemen")
-#table(table(df5$Species)<5)
+  x.freq.4 <- aggregate(Fre ~ Year, x.freq.3, function(x)
+    sum(x))
+  x.freq.5 <- merge(x.freq.3, x.freq.4, by = "Year")
+  x.freq.5 <- x.freq.5[order(x.freq.5$Species, x.freq.5$Year),]
+
+  x.freq.5 <- x.freq.5[c(2, 1, 3, 4, 5)]
+  colnames(x.freq.5) <-
+    c("Species", "Year", "COUNT", "CUM_FREQ", "No_specimen_year")
+  timeseries <- x.freq.5[order(x.freq.5$Species, x.freq.5$Year),]
+  return(timeseries)
+
+  # annual frequency counts (survey effort)
+  x.freq.6 <-
+    as.data.frame(seq.int(from = (x.freq.4[1, 1]), to = (x.freq.4[as.numeric(length(x.freq.4$Fre)), 1])))
+  colnames(x.freq.6)[1] <- "Year"
+
+  x.freq.7 <- merge(x.freq.4, x.freq.6, all = TRUE, by = "Year")
+  x.freq.7$Fre[is.na(x.freq.7$Fre)] <- 0
+  colnames(x.freq.7) <- c("Year", "No_speciemen")
+  frequency <- x.freq.7
+  return(frequency)
 }
--- README.md
+++ README.md
@@ -5,28 +5,30 @@ output:
   pdf_document: default
   word_document: default
 bibliography: bibliography.bib
+csl: apa-5th.csl
 ---
 
 
 
-This package fits glms over species time series to detect time lag in species population growth. The code is based on @Hyndman2015-rt and the code published on
+This package fits glms over species time series to detect time lag in species population growth. The code is based on @Hyndman2015-rt and the code published on [www.robjhyndman.com](https://robjhyndman.com/Rfiles/lagphase.R).
 
 
 
 
 ## Installation
-You can install lag detection package from github with:
+You can install the lag detection package from github with:
 
 ``` r
 # install.packages("devtools")
 devtools::install_github("PhillRob/lag-package")
+library(lag-package)
 ```
 
 ## Example
 This is a basic example which shows you how to solve a common problem:
 
 ``` r
-## basic example code
+lagTest <- runlag(x = TimeSeriesNatPlantNZ,y = AnnualFrequencyNatPlantNZ)
 ```
 
 
@@ -36,7 +38,7 @@ Invasive species time series from NZ is included as test data is included from @
 load("data/AnnualFrequencyNatPlantNZ.rda")
 load("data/TimeSeriesNatPlantNZ.rda")
 ```
-You can use **filename** to wrap **GBIF** data into the format required by the code. 
+You can use `gbifwranger.R` to wrap [GBIF](https://www.gbif.org/) data downloaded using the [rgbif package](https://cran.r-project.org/web/packages/rgbif/rgbif.pdf) [@Scott_Chamberlain_Vijay_Barve_Dan_Mcglinn_Damiano_Oldoni_Laurens_Geffert_Karthik_Ram2018-zi] into the format required by the lag code. 
 
 
 
